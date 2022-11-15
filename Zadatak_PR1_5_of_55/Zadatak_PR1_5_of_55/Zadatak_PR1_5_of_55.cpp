ceb07dd3ecaac152307dd690c1d095ab5a558700
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-11-15 21:20:44 +0100

    added Zadatak 5 of 55

---
+++ Zadatak_PR1_5_of_55/Zadatak_PR1_5_of_55/Zadatak_PR1_5_of_55.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*Napisati program koji ce omoguciti unos prirodnog broja A (A > 100000), te kreirati i ispisati broj B koji
+ce se sastojati od cifara broja A u obrnutom redoslijedu u kojem je svaka parna uklonjena.
+(npr. ako se unese 45362792 funkcija ce vratiti 9735). Ispisati razliku brojeva A i B.
+
+(Obavezno koristiti bar jednu korisnicki definiranu funkciju.)
+*/
+
+int main() {
+	return 0;
+}
