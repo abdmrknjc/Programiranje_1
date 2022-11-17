6fae374707a0f0ef0ae8ec06c08c69c3310842d1
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-11-18 00:31:33 +0100

    edited zadatak 6 of 55

--- Zadatak_PR1_6_of_55/Zadatak_PR1_6_of_55/Zadatak_PR1_6_of_55.cpp
+++ Zadatak_PR1_6_of_55/Zadatak_PR1_6_of_55/Zadatak_PR1_6_of_55.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-
 using namespace std;
+
 /*Postujuci sve faze procesa programiranja, napisati program koji korisniku omogucava unos cijelog broja N,
 te izracunava vrijednost sume faktorijela neparnih brojeva u intervalu [1, N].
 Suma faktorijela prikazana je sljedecom formulom: S = 1! + 3! + 5! + ... + N!*/
 
-int SumaFaktorijela(int);
-
+int sumaFaktorijela(int);
 int faktorijel(int);
 
 int main() {
 	int N;
-	cout << "Unesite neki cijeli broj: " << endl;
+	cout << "Unesite cijeli broj: " << endl;
 	cin >> N;
-	cout << "Suma faktorijela svih neparnih brojeva do unesenog broja je: " << SumaFaktorijela(N) << endl;
-
+	cout << "Suma faktorijela neparnih brojeva do unesenog broja je: " << sumaFaktorijela(N);
 }
-int SumaFaktorijela(int N) {
+
+int sumaFaktorijela(int N) {
 	int suma = 0;
-	for (int i = 1; i <= N; i += 2)//Kontrola petlje da se samo krece po neparnim;
+	for (int i = 1; i <= N; i += 2)
 	{
 		suma += faktorijel(i);
 	}
 	return suma;
 }
+
 int faktorijel(int broj) {
 	int Faktorijel = 1;
 	for (int i = 1; i <= broj; i++)
 	{
-		Faktorijel = Faktorijel * i;
+		Faktorijel *= i;
 	}
 	return Faktorijel;
 }
