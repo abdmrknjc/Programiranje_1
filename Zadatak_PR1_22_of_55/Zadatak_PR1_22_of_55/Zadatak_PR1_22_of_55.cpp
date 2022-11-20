e3d8bcee6e73dc90781fd649c73816508c38de5a
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-11-20 17:42:39 +0100

    started zadatak 22 of 55

---
+++ Zadatak_PR1_22_of_55/Zadatak_PR1_22_of_55/Zadatak_PR1_22_of_55.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+/*Napisati program koji izracunava ukupan zbir faktorijela od svih prostih brojeva
+koji su clanovi prvih 100 elemenata Fibonacci niza (tzv. fibonacci prime).
+
+Napomena: Pazljivo procitajte zadatak. Obavezno voditi racuna da se uradi tacno ono
+sto je trazeno u zadatku i da zadatak bude logicki ispravan.
+Obavezno koristiti bar jednu funkciju kojoj cete proslijediti parametar po referenci.
+*/
+
+int main() {
+	return 0;
+}
